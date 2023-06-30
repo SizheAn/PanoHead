106083cb37e456dbbf2893f3017477a8b1634bb2
Yichun Shi <sc2h6o@gmail.com>
2023-06-30 01:40:00 -0700

    update file descriptions

--- projector_withseg.py
+++ projector_withseg.py
@@ -1,12 +1,4 @@
-# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-
-"""Project given image to the latent space of pretrained network pickle."""
+""" Projecting input images into latent spaces. """
 
 import copy
 import os
